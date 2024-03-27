6502ccda83fc7b391d7bf16077c4afeb982ac215
ibizaman <ibizapeanut@gmail.com>
2024-03-26 20:41:36 -0700

    add installer with test in vm

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Skarabox
+
+## Why?
+
+Install NixOS on a server in two steps.
+
+- The disk will be formatted to use ZFS.
+- [Self Host Blocks](https://github.com/ibizaman/selfhostblocks) project will be used to install services.
+
+## Caution
+
+Following the steps WILL ERASE THE CONTENT of any disk on that server.
+
+## Usage
+
+1. Boot on the NixOS installer. You just need to boot, no need to install.
+
+   1. First, create the .iso file.
+
+   ```
+   nix build .#beacon
+   ```
+
+   2. Copy the .iso file to a USB key. This WILL ERASE THE CONTENT of the USB key.
+
+   ```
+   nix run nixpkgs#usbimager
+   ```
+
+   - Select `./result/iso/beacon.iso` file in row 1 (`...`).
+   - Select USB key in row 3.
+   - Click write (arrow down) in row 2.
+
+   3. Plug USB in the server. Choose to boot on it.
+
+   You will be logged in automatically with user `nixos`.
+
+## Contribute
+
+To start a VM with the beacon, run:
+
+```
+nix run .#beacon-test
+```
+
+## Links
+
+- https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/installation-device.nix
+- https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/cd-dvd/installation-cd-base.nix
+- https://github.com/nix-community/nixos-anywhere/blob/main/docs/howtos/no-os.md#installing-on-a-machine-with-no-operating-system
---
+++ installNixOs/flake.nix
@@ -0,0 +1,98 @@
+{
+  description = "Skarabox's flake to install NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.x86_64-linux = {
+      beacon = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        format = "install-iso";
+
+        modules = [({ config, lib, ... }: {
+          users.users.nixos.initialHashedPassword = lib.mkForce "$y$j9T$7EZvmryvlpTHSRG7dC5IU1$lBc/nePnkvqZ//jNpx/UpFKze/p6P7AIhJubK/Ghj68";
+
+          isoImage.isoName = lib.mkForce "beacon.iso";
+
+          networking.firewall.allowedTCPPorts = [ 22 ];
+
+          services.getty.helpLine = lib.mkForce ''
+
+              /           \\
+             |/  _.-=-._  \\|       SKARABOX
+             \\'_/`-. .-'\\_'/ 
+              '-\\ _ V _ /-'
+                .' 'v' '.     Hello, you just booted on the Skarabox beacon.
+              .'|   |   |'.   Congratulations!
+              v'|   |   |'v
+                |   |   |     Nothing is yet installed on this server. To abort, just
+               .\\   |   /.    close this server and remove the USB stick.
+              (_.'._^_.'._)   
+               \\\\       //    To complete the installation of Skarabox on this server, you
+                \\'-   -'/     must follow the steps below to run the Skarabox installer.
+
+
+             WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+             *                                                                           *
+             *    Running the Skarabox installer WILL ERASE EVERYTHING on this server.   *
+             *    Make sure the only disk connected and powered on is the disk to        *
+             *    install the Operating System on. This disk should be a SSD or NVMe     *
+             *    disk for optimal performance. THIS DISK WILL BE ERASED.                *
+             *                                                                           *
+             WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+
+
+            * Step 1.  Enable network access to this server. 
+
+            A wired network connection is available if you connect an Ethernet cable. If you
+            need a wireless connection, configure a network by typing the command "wpa_cli"
+            without the enclosing double quotes.
+
+            * Step 2.  Run the installer.
+
+            When running the installer, you will need to enter the password "skarabox123" as
+            well as the IP address of this server. To know the IP address, first follow the
+            first step then type the command "ip -brief a" verbatim, without the enclosing
+            double quotes.
+
+            Try all IP addresses one by one until one works. An IP address looks like so:
+
+              192.168.1.15
+              10.0.2.15
+
+            * Step 3.  Reboot this server and remove the USB stick.
+          '';
+        })];
+      };
+
+      beacon-test = let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        iso = "${self.outputs.packages.${system}.beacon}/iso/beacon.iso";
+        hostSshPort = 2222;
+      in (pkgs.writeShellScriptBin "runner.sh" ''
+        ${pkgs.qemu}/bin/qemu-system-x86_64 \
+          -m 2048M \
+          -nic hostfwd=tcp::${toString hostSshPort}-:22 \
+          --drive media=cdrom,format=raw,readonly=on,file=${iso}
+        
+        '');
+    };
+
+    apps.x86_64-linux = {
+      beacon-test = {
+        type = "app";
+        program = "${self.outputs.packages.x86_64-linux.beacon-test}/bin/runner.sh";
+      };
+    };
+  };
+}
