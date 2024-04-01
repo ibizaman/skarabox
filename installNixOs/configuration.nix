6e1290c6296d33e1f20751b9e70b0af4442361d8
ibizaman <ibizapeanut@gmail.com>
2024-03-31 22:39:23 -0700

    do not generate hostId yet

--- installNixOs/configuration.nix
+++ installNixOs/configuration.nix
@@ -21,6 +21,11 @@ in
       default = "$y$j9T$7EZvmryvlpTHSRG7dC5IU1$lBc/nePnkvqZ//jNpx/UpFKze/p6P7AIhJubK/Ghj68";
       description = "Initial password for the admin user. Can be changed later. Default is 'skarabox123'.";
     };
+
+    hostId = lib.mkOption {
+      type = lib.types.str;
+      description = "8 characters unique identifier for this server. Generate with `uuidgen | head -c 8`.";
+    };
   };
 
   config = {
@@ -28,12 +33,14 @@ in
     boot.loader.efi.canTouchEfiVariables = true;
 
     networking.hostName = cfg.hostname;
-    networking.hostId = lib.mkDefault
-      (pkgs.lib.readFile ((pkgs.runCommand "hostid.sh" {}
-        ''
-          mkdir -p $out
-          ${pkgs.util-linux}/bin/uuidgen | head -c 8 > $out/hostid
-        '') + "/hostid"));
+    networking.hostId = cfg.hostId;
+      # TODO: this was a good idea but generating a new hostId every time is not what's needed. The
+      # generation should only happen once.
+      # (pkgs.lib.readFile ((pkgs.runCommand "hostid.sh" {}
+      #   ''
+      #     mkdir -p $out
+      #     ${pkgs.util-linux}/bin/uuidgen | head -c 8 > $out/hostid
+      #   '') + "/hostid"));
     networking.useDHCP = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
     nixpkgs.hostPlatform = "x86_64-linux";
