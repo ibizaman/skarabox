72b7f682aa839805f3c3a8bb40f96403f2713fe2
ibizaman <ibizapeanut@gmail.com>
2025-04-15 14:43:56 +0200

    wait for VM to be ready in tests

--- tests/default.nix
+++ tests/default.nix
@@ -33,17 +33,45 @@
     ${nix} run ${../.}#demo-install-on-beacon 127.0.0.1 2222 ${../.}
     e "Installation succeeded."
 
+    e "Starting ssh loop to figure out when VM is ready to receive root passphrase."
+    e "You might see some flickering on the command line."
+    while ! ${nix} run ${../.}#demo-ssh 127.0.0.1 2223 root echo "connected" 2>/dev/null; do
+      sleep 5
+    done
+    e "Beacon VM has started."
+
     e "Decrypting root dataset."
     printf "rootpassphrase" | ${nix} run ${../.}#demo-ssh 127.0.0.1 2223 root
     e "Decryption done."
+
+    e "Starting ssh loop to figure out when VM has booted."
+    e "You might see some flickering on the command line."
+    while ! ${nix} run ${../.}#demo-ssh 127.0.0.1 2222 skarabox echo "connected" 2>/dev/null; do
+      sleep 5
+    done
+    e "Beacon VM has started."
 
     e "Rebooting to confirm we can connect after a reboot."
     ${nix} run ${../.}#demo-ssh 127.0.0.1 2222 skarabox sudo reboot
     e "Rebooting in progress."
 
+    e "Starting ssh loop to figure out when VM is ready to receive root passphrase."
+    e "You might see some flickering on the command line."
+    while ! ${nix} run ${../.}#demo-ssh 127.0.0.1 2223 root echo "connected" 2>/dev/null; do
+      sleep 5
+    done
+    e "Beacon VM has started."
+
     e "Decrypting root dataset."
     printf "rootpassphrase" | ${nix} run ${../.}#demo-ssh 127.0.0.1 2223 root
     e "Decryption done."
+
+    e "Starting ssh loop to figure out when VM has booted."
+    e "You might see some flickering on the command line."
+    while ! ${nix} run ${../.}#demo-ssh 127.0.0.1 2222 skarabox echo "connected" 2>/dev/null; do
+      sleep 5
+    done
+    e "Beacon VM has started."
 
     e "Connecting and shutting down"
     ${nix} run ${../.}#demo-ssh 127.0.0.1 2222 skarabox sudo shutdown
