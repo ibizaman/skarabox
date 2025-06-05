9a74bf18454d82922d077cfb7fa6eb9fd1233ee0
ibizaman <ibizaman@tiserbox.com>
2025-06-05 22:05:28 +0200

    allow to copy extra secrets on install

--- lib/install-on-beacon.nix
+++ lib/install-on-beacon.nix
@@ -5,24 +5,19 @@ pkgs.writeShellApplication {
   runtimeInputs = [
     nixos-anywhere
     pkgs.bash
-    pkgs.sops
   ];
 
   text = ''
     usage () {
       cat <<USAGE
-    Usage: $0 -i IP -p PORT -f FLAKE -k HOST_KEY -r ROOT_PASSPHRASE_FILE -d DATA_PASSPHRASE_FILE [-a EXTRA_OPTS]
+    Usage: $0 -i IP -p PORT -f FLAKE -k HOST_KEY_FILE [-a EXTRA_OPTS]
 
-      -h:                       Shows this usage
-      -i IP:                    IP of the target host running the beacon.
-      -p PORT:                  Port of the target host running the beacon.
-      -f FLAKE:                 Flake to install on the target host.
-      -k HOST_KEY_FILE:         SSH key to use as the host identification key.
-      -r ROOT_PASSPHRASE_PATH:  Path in the yaml secrets file for the root passphrase used to encrypt the root ZFS pool.
-      -d DATA_PASSPHRASE_PATH:  Path in the yaml secrets file for the data passphrase used to encrypt the root ZFS pool.
-      -s SOPS_KEY:              File containing a sops key capable of decrypting the secrets.
-      -e SECRETS_FILE:          File containing the secrets.
-      -a EXTRA_OPTS:            Extra options to pass verbatim to nixos-anywhere.
+      -h:                        Shows this usage
+      -i IP:                     IP of the target host running the beacon.
+      -p PORT:                   Port of the target host running the beacon.
+      -f FLAKE:                  Flake to install on the target host.
+      -k HOST_KEY_FILE:          SSH key to use as the host identification key.
+      -a EXTRA_OPTS:             Extra options to pass verbatim to nixos-anywhere.
     USAGE
     }
 
@@ -32,7 +27,7 @@ pkgs.writeShellApplication {
       fi
     }
 
-    while getopts "hi:p:f:k:r:d:s:e:a:" o; do
+    while getopts "hi:p:f:k:d:a:" o; do
       case "''${o}" in
         h)
           usage
@@ -50,18 +45,6 @@ pkgs.writeShellApplication {
         k)
           host_key_file=''${OPTARG}
           ;;
-        r)
-          root_passphrase_path=''${OPTARG}
-          ;;
-        d)
-          data_passphrase_path=''${OPTARG}
-          ;;
-        s)
-          sopskey=''${OPTARG}
-          ;;
-        e)
-          secretsfile=''${OPTARG}
-          ;;
         a)
           read -ra extra_opts <<< "''${OPTARG}"
           ;;
@@ -77,20 +60,10 @@ pkgs.writeShellApplication {
     check_empty "$port" -p port
     check_empty "$flake" -f flake
     check_empty "$host_key_file" -k host_key_file
-    check_empty "$root_passphrase_path" -r root_passphrase_path
-    check_empty "$data_passphrase_path" -d data_passphrase_path
-    check_empty "$sopskey" -s sopskey
-    check_empty "$secretsfile" -e secretsfile
-
-    export SOPS_AGE_KEY_FILE=$sopskey
-    root_passphrase=$(sops decrypt --extract "$root_passphrase_path" "$secretsfile")
-    data_passphrase=$(sops decrypt --extract "$data_passphrase_path" "$secretsfile")
 
     nixos-anywhere \
       --flake "$flake" \
       --disk-encryption-keys /tmp/host_key "$host_key_file" \
-      --disk-encryption-keys /tmp/root_passphrase <(echo "$root_passphrase") \
-      --disk-encryption-keys /tmp/data_passphrase <(echo "$data_passphrase") \
       --ssh-port "$port" \
       "''${extra_opts[@]}" \
       skarabox@"$ip"
