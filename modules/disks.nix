db0c5dd9dc4820b0ba923fbe2fbb491c4b5bcbd4
ibizaman <ibizaman@tiserbox.com>
2025-06-10 23:28:42 +0200

    split boot ssh config in its own file

--- modules/disks.nix
+++ modules/disks.nix
@@ -2,14 +2,7 @@
 let
   cfg = config.skarabox.disks;
 
-  inherit (lib) isString mkIf mkOption optionals optionalString toInt types;
-
-  readAndTrim = f: lib.strings.trim (builtins.readFile f);
-  readAsStr = v: if lib.isPath v then readAndTrim v else v;
-  readAsInt = v: let
-    vStr = readAsStr v;
-  in
-    if lib.isString vStr then toInt vStr else vStr;
+  inherit (lib) mkIf mkOption optionals optionalString types;
 in
 {
   options.skarabox.disks = {
@@ -104,62 +97,6 @@ in
       default = true;
     };
 
-    boot = mkOption {
-      type = types.submodule {
-        options = {
-          sshPort = mkOption {
-            type = with types; oneOf [ int str path ];
-            description = "Port the SSH daemon used to decrypt the root partition listens to.";
-            default = 2222;
-            apply = readAsInt;
-          };
-
-          staticNetwork = mkOption {
-            description = "Use static IP configuration. If unset, use DHCP.";
-            default = null;
-            type = types.nullOr (types.submodule {
-              options = {
-                ip = mkOption {
-                  type = types.str;
-                  description = "Static IP to use.";
-                };
-                gateway = mkOption {
-                  type = types.str;
-                  description = "IP Gateway, often `XXX.YYY.ZZZ.1`.";
-                };
-                netmask = mkOption {
-                  type = types.str;
-                  description = "Netmask of local network.";
-                  default = "255.255.255.0";
-                };
-                device = mkOption {
-                  description = ''
-                  Device for which to configure the IP address for.
-
-                  Either pass the device name directly if you know it, like "ens3".
-                  Or configure the `subClass` option to get the first device name
-                  matching that sub-class from the facter.json report.
-                  '';
-                  default = { subClass = "Ethernet"; };
-                  type = with types; oneOf [
-                    str
-                    (submodule {
-                      options = {
-                        subClass = mkOption {
-                          type = str;
-                          description = "Sub-class as it appears in the facter.json report.";
-                          default = "Ethernet";
-                        };
-                      };
-                    })
-                  ];
-                };
-              };
-            });
-          };
-        };
-     };
-   };
 
 
  };
@@ -381,6 +318,13 @@ in
       "/boot".neededForBoot = true;
       "/boot-backup" = mkIf (cfg.rootPool.disk2 != null) { neededForBoot = true; };
     };
+
+    # Follows https://grahamc.com/blog/erase-your-darlings/
+    # https://github.com/NixOS/nixpkgs/pull/346247/files
+    boot.initrd.postResumeCommands = lib.mkAfter ''
+      zfs rollback -r ${cfg.rootPool.name}/local/root@blank
+    '';
+
     # Setup Grub to support UEFI.
     # nodev is for UEFI.
     boot.loader.grub = {
@@ -400,53 +344,6 @@ in
         }
       ]));
     };
-
-    # Follows https://grahamc.com/blog/erase-your-darlings/
-    # https://github.com/NixOS/nixpkgs/pull/346247/files
-    boot.initrd.postResumeCommands = lib.mkAfter ''
-      zfs rollback -r ${cfg.rootPool.name}/local/root@blank
-    '';
-
-    # Enables DHCP in stage-1 even if networking.useDHCP is false.
-    boot.initrd.network.udhcpc.enable = lib.mkDefault (cfg.boot.staticNetwork == null);
-    # From https://wiki.nixos.org/wiki/ZFS#Remote_unlock
-    boot.initrd.network = {
-      # This will use udhcp to get an ip address. Nixos-facter should have found the correct drivers
-      # to load but in case not, they need to be added to `boot.initrd.availableKernelModules`.
-      # Static ip addresses might be configured using the ip argument in kernel command line:
-      # https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
-      enable = true;
-      ssh = {
-        enable = true;
-        # To prevent ssh clients from freaking out because a different host key is used,
-        # a different port for ssh is used.
-        port = lib.mkDefault cfg.boot.sshPort;
-        hostKeys = lib.mkForce ([ "/boot/host_key" ] ++ (optionals (cfg.rootPool.disk2 != null) [ "/boot-backup/host_key" ]));
-        # Public ssh key used for login.
-        # This should contain just one line and removing the trailing
-        # newline could be fixed with a removeSuffix call but treating
-        # it as a file containing multiple lines makes this forward compatible.
-        authorizedKeyFiles = [
-          config.skarabox.sshAuthorizedKeyFile
-        ];
-      };
-
-      postCommands = ''
-      zpool import -a
-      echo "zfs load-key ${cfg.rootPool.name}; killall zfs; exit" >> /root/.profile
-      '';
-    };
-    boot.kernelParams = lib.optionals (cfg.boot.staticNetwork != null && config.facter.report != {}) (let
-        n = cfg.boot.staticNetwork;
-
-        firstMatchingDevice = subClass: (builtins.head (builtins.filter (n: n.sub_class.name == subClass) config.facter.report.hardware.network_interface)).unix_device_name;
-
-        deviceName = if isString n.device then n.device else firstMatchingDevice n.device.subClass;
-      in [
-        # https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
-        # ip=<client-ip>:<server-ip>:<gw-ip>:<netmask>:<hostname>:<device>:<autoconf>:<dns0-ip>:<dns1-ip>:<ntp0-ip>
-        "ip=${n.ip}::${n.gateway}:${n.netmask}:${config.skarabox.hostname}-initrd:${deviceName}:off:::"
-      ]);
 
     services.zfs.autoScrub.enable = true;
   };
