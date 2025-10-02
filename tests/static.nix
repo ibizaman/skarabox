b11da0549693375e18ea14884f15aa8e6c3e9da3
ibizaman <ibizaman@tiserbox.com>
2025-10-02 16:58:39 +0200

    remove use of separate ssh port files

--- tests/static.nix
+++ tests/static.nix
@@ -59,8 +59,6 @@
 
     group "Initialising template"
     echo skarabox1234 | ${nix} run ${../.}#init -- -n myskarabox -v -y -s -p ${../.}
-    echo 2223 > ./myskarabox/ssh_boot_port
-    echo 2222 > ./myskarabox/ssh_port
     echo 127.0.0.1 > ./myskarabox/ip
     echo ${system} > ./myskarabox/system
     ${nix} run .#myskarabox-gen-knownhosts-file
